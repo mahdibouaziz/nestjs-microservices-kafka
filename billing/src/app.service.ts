9de8ffdcb46490527256186e9f5b367a0e24c442
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-27 12:15:14 +0100

    consume the messages in the billing from the api-gateway

--- billing/src/app.service.ts
+++ billing/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { OrderCreatedEvent } from './order-created.event';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleOrderCreated(orderCreatedEvent: OrderCreatedEvent) {
+    // receive the event
+    console.log(orderCreatedEvent);
+    // go to the auth service and get the user associated with this event
   }
 }
