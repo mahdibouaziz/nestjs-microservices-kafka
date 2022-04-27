07c4340c31973df2462c75a21d8630d1a793f4e7
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-27 12:37:59 +0100

    consume messages

--- auth/src/app.service.ts
+++ auth/src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { GetUserRequest } from './get-user-request.dto';
 
 @Injectable()
 export class AppService {
+  private readonly users = [
+    {
+      userId: '123',
+      stripeUserId: '45625',
+    },
+    {
+      userId: '124',
+      stripeUserId: '99865',
+    },
+  ];
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUser(getUserRequest: GetUserRequest) {
+    console.log(getUserRequest);
+    return this.users.find((user) => user.userId === getUserRequest.userId);
   }
 }
