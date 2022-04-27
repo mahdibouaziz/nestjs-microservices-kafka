c247e770b514aa5ddc7942b6d03bea99339fe735
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-27 11:57:46 +0100

    configured the api gateway emit events to the billing service

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateOrderRequest } from './create-order-request.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  createOrder(@Body() createOrderRequest: CreateOrderRequest) {
+    this.appService.createOrder(createOrderRequest);
   }
 }
--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { CreateOrderRequest } from './create-order-request.dto';
+import { OrderCreatedEvent } from './order-created.event';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('BILLING_SERVICE') private readonly billingClient: ClientKafka,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createOrder({ userId, price }: CreateOrderRequest) {
+    // emit an event to the billing service
+    this.billingClient.emit(
+      'order_created',
+      new OrderCreatedEvent('123', userId, price),
+    );
   }
 }
